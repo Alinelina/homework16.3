a3f5bf5bdcca6017449f586d11d07dfc5d7c8c31
Alina <91183241+Alinelina@users.noreply.github.com>
2022-06-16 13:14:34 +0300

    corrected main.js

--- main.js
+++ main.js
@@ -2,13 +2,5 @@ let firstElemInArray = document.querySelector('.first-element');
 firstElemInArray.innerHTML = ['js', 'css', 'html'][0];
 
 
-let result = document.querySelector('.result');
-result.innerHTML = [0, 1, false, 2, undefined, '', 3, null].filter(elem => {
-    elem !== false; 
-    return elem;
-});
-
-
-//Можете, пожалуйста, объяснить почему без return возвращает другой ответ?
-// result.innerHTML = [0, 1, false, 2, undefined, '', 3, null].filter(elem => elem !== false);
-// 0,1,2,,,3,
+const result = document.querySelector('.result');
+result.innerHTML = [0, 1, false, 2, undefined, '', 3, null].filter(elem => elem);
